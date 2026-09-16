14a3788f1d5f763d82200b63269e06009adf3b75
RafaelTabolka <rafaeltabolka@hotmail.com>
2026-09-16 17:02:30 -0300

    Adicionado comando de criar da entidade cliente

---
+++ src/TurnoDoSiriCascudo.Dominio/Comandos/ClienteComandos/Criar/ClienteCriarHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using TurnoDoSiriCascudo.Dominio.Entidades.ClienteEntidade;
+using TurnoDoSiriCascudo.Dominio.Interfaces.IRepositorio.ClienteRepositorio;
+
+namespace TurnoDoSiriCascudo.Dominio.Comandos.ClienteComandos.Criar
+{
+    internal class ClienteCriarHandler(IRepositorioCliente repositorioCliente) :
+        IRequestHandler<ClienteCriarRequest, ClienteCriarResponse>
+    {
+        public async Task<ClienteCriarResponse> Handle(
+            ClienteCriarRequest request, CancellationToken cancellationToken)
+        {
+            var cliente = new Cliente(
+                request.NomeCliente,
+                request.Telefone,
+                request.Observacoes,
+                request.UsuarioCriadorId
+            );
+
+            await repositorioCliente.AdicionarAsync(cliente);
+            await repositorioCliente.CommitAsync();
+
+            return new ClienteCriarResponse(cliente.Id, "Cliente criado com sucesso");
+        }
+    }
+}
---
+++ src/TurnoDoSiriCascudo.WebApi/Controllers/ClienteControllers/ClienteController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TurnoDoSiriCascudo.Dominio.Comandos.ClienteComandos.Criar;
+
+namespace TurnoDoSiriCascudo.WebApi.Controllers.ClienteControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController(ISender mediator) : ControllerBase
+    {
+        [HttpPost("criar")]
+        public async Task<IActionResult> CriarCliente(ClienteCriarRequest request)
+        {
+            var response = await mediator.Send(request);
+
+            return Created("Criado", response);
+        }
+    }
+}
